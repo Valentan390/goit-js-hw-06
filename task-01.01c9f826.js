af62b0ca58f0898a1c1dd1f57ebbe22060b8ca5a
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-08-26 09:39:33 +0000

    Deploying to gh-pages from @ Valentan390/goit-js-hw-06@b91dc4c3894f51ee7e562b0ec6155687908cc65e 🚀

---
+++ task-01.01c9f826.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#categories");console.log("Number of categories: ".concat(e.children.length)),document.querySelectorAll(".item").forEach((function(e){var t=e.firstElementChild.textContent,n=e.lastElementChild.children.length;return console.log("Category: ".concat(t," \nElements: ").concat(n))}))}();
+//# sourceMappingURL=task-01.01c9f826.js.map
---
+++ task-01.206b7037.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#categories");console.log(`Number of categories: ${e.children.length}`);document.querySelectorAll(".item").forEach((e=>{let t=e.firstElementChild.textContent,l=e.lastElementChild.children.length;return console.log(`Category: ${t} \nElements: ${l}`)}));
+//# sourceMappingURL=task-01.206b7037.js.map
