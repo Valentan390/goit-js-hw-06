b91dc4c3894f51ee7e562b0ec6155687908cc65e
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-08-26 12:39:06 +0300

    Did the first job

---
+++ src/js/task-01.js
@@ -0,0 +1,11 @@
+const galleryListEl = document.querySelector('#categories');
+console.log(`Number of categories: ${galleryListEl.children.length}`);
+
+const itemEl = document.querySelectorAll('.item');
+itemEl.forEach((elem) => {
+    let category = elem.firstElementChild.textContent;
+    let elements = elem.lastElementChild.children.length;
+    return console.log(`Category: ${category} \nElements: ${elements}`);
+    });
+
+
