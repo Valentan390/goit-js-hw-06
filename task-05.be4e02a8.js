c4213674a0db99c852bd1a5ac508b2daba92e810
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-08-27 07:48:50 +0000

    Deploying to gh-pages from @ Valentan390/goit-js-hw-06@fd8822c23252c329e04bf9c90fd5c2b5d1d3e358 🚀

---
+++ task-05.be4e02a8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#name-input");console.log(e);var t=document.querySelector("#name-output");console.log(t);e.addEventListener("input",(function(e){e.currentTarget.value.length>0?t.textContent=e.currentTarget.value:t.textContent="Anonymous"}))}();
+//# sourceMappingURL=task-05.be4e02a8.js.map
